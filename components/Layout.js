a739804fa35e616c73e53aa00280444b79d3e4cc
kzisopoulos <k91.zis@gmail.com>
2022-05-29 11:44:05 +0300

    head + favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-
+import Head from 'next/head';
 const Layout = ({ children }) => {
   return (
     <div>
+      <Head>
+        <meta
+          name="Description"
+          content="This is the personal portfolio of Konstantinos Zisopoulos an aspiring web developer and infrastructure engineer. It contains information about knowledge and latest projects plus a way of contact."
+        ></meta>
+        <title>Kostas Zisopoulos | Web & IT</title>
+      </Head>
       <Navbar />
       {children}
       <Footer />
